dbbc35b22554807abf4eba723a360f4ea53ae1ca
Cam Graff <graffcameron@gmail.com>
2021-12-17 21:42:32 -0600

    Implement message passing with fuzzy search

--- tmux-schmooze/tmux-textual.py
+++ tmux-schmooze/tmux-textual.py
@@ -1,6 +1,8 @@
 import string
 from rich.control import Control
 from rich.segment import ControlType
+from textual.layouts.dock import Dock
+from textual.message import Message
 from .tmux import GridArea, parse_layout
 from logging import PlaceHolder
 from typing import Iterable, List
@@ -19,6 +21,36 @@ from textual.view import View
 from textual.widget import Widget
 from textual.widgets import Static
 from textual.widgets import Placeholder
+from textual.views import DockView
+from textual.message import MessageTarget
+from fuzzyfinder.main import fuzzyfinder
+
+class InputChanged(Message):
+    def __init__(self, sender: MessageTarget, value: str) -> None:
+        self.value = value
+        super().__init__(sender)
+
+class FuzzyFinder(DockView):
+    def __init__(self, candidates: List[str], name: str | None = None) -> None:
+        super().__init__(name=name)
+        self.picker = Picker()
+        self.input = TextInput()
+        self.candidates = candidates
+
+    async def handle_input_changed(self, event: InputChanged):
+        res = fuzzyfinder(event.value, self.candidates)
+        print(list(res))
+
+    async def on_mount(self, event: events.Mount) -> None:
+        await self.dock(self.picker, edge="top", size=round(self.console.height*0.9))
+        await self.dock(self.input, edge="bottom")
+        return await super().on_mount(event)
+
+
+class Picker(Widget):
+
+    def render(self) -> RenderableType:
+        return super().render()
 
 class TextInput(Widget):
     def __init__(self, name: str | None = None) -> None:
@@ -49,6 +81,7 @@ class TextInput(Widget):
         elif event.key in string.printable:
             self.value += event.key
             self._cursor_position += 1
+        await self.emit(InputChanged(self, self.value))
         self.refresh()
 
     def __rich__(self) -> RenderableType:
@@ -91,28 +124,15 @@ class PaneLayout(Layout):
 class MyApp(App):
     async def on_key(self, event: events.Key) -> None:
         await super().on_key(event)
-        await self.input.on_key(event)
+        await self.fuzzy_finder.input.on_key(event)
 
     async def on_load(self, event: events.Load) -> None:
         """Bind keys with the app loads (but before entering application mode)"""
-        await self.bind("b", "view.toggle('sidebar')", "Toggle sidebar")
         await self.bind("q", "quit", "Quit")
-        self.input = TextInput()
+        sessions = subprocess.getoutput("tmux list-sessions -F '#S'").splitlines()
+        self.fuzzy_finder = FuzzyFinder(sessions)
 
     async def on_mount(self, event: events.Mount) -> None:
-        left = round(self.console.size.width * 0.2)
-        grid = await self.view.dock_grid()
-        grid.add_column("left", size=left)
-        grid.add_column("right")
-        grid.add_row("top", size=round(self.console.height*0.9))
-        grid.add_row("bottom")
-
-        grid.add_areas(
-            picker="left,top",
-            text_input="left,bottom",
-            panes="right,top-start|bottom-end"
-        )
-
         layout = PaneLayout(0.8)
         layout_str = subprocess.getoutput("tmux display-message -p -F '#{window_visible_layout}' -t lila:0")
         layouts = parse_layout(layout_str)
@@ -121,10 +141,7 @@ class MyApp(App):
             layout.add_pane(Pane(l, Text.from_ansi(pane_content, no_wrap=True, end="")))
         panes = View(layout, name="panes")
         # TODO: Figure out how to keep the proportions on window resize
-        grid.place(
-            picker=Placeholder(),
-            text_input=self.input,
-            panes=panes
-        )
+        await self.view.dock(self.fuzzy_finder, edge="left", size=round(self.console.size.width * 0.2))
+        await self.view.dock(panes, edge="right")
 
 MyApp.run(title="Simple App", log="textual.log")
